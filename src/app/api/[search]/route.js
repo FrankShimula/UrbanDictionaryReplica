69e52eaa0f2c86113bfc727accaeb19d913d3981
Frank Shimula <josephmuller53@gmail.com>
2024-12-06 20:35:18 +0300

    s.error

--- src/app/api/[search]/route.js
+++ src/app/api/[search]/route.js
@@ -3,26 +3,29 @@ import dbConnect from "@/utils/mongodb";
 import Terms from "@/model/Terms";
 
 export async function GET(req) {
-    await dbConnect();
-
-    const { searchParams } = new URL(req.url);
-    const query = searchParams.get("query");
-
-    if (!query) {
-        return NextResponse.json({ message: "Query is required" }, { status: 400 });
-    }
-
     try {
-        const words = await Terms.find({
-            word: { $regex: query, $options: "i" },
-        }).limit(20);
+        await dbConnect();
 
-        return NextResponse.json({ words }, { status: 200 });
-    } catch (err) {
-        console.error("Search error:", err);
-        if (err instanceof Error) {
-            return NextResponse.json({ message: err.message }, { status: 500 });
+        const { searchParams } = new URL(req.url);
+        const query = searchParams.get("query");
+
+        if (!query) {
+            return NextResponse.json({ message: "Query is required" }, { status: 400 });
         }
-        return NextResponse.json({ message: "Unknown error occurred" }, { status: 500 });
+
+        try {
+            const words = await Terms.find({
+                word: { $regex: query, $options: "i" },
+            }).limit(20);
+
+            return NextResponse.json({ words }, { status: 200 });
+        } catch (err) {
+            console.error("Search error:", err);
+            return NextResponse.json({ message: "Server error" }, { status: 500 });
+        }
+
+    }
+    catch (err) {
+        return NextResponse.json({ message: "Database connection error" }, { status: 500 });
     }
 }
